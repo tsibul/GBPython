543b8e5a355f7a7cdaf72189fe9e42fe7dd99608
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-21 22:01:28 +0300

    final seminar

--- GBS_1_211122.py
+++ GBS_1_211122.py
@@ -1,14 +1,17 @@
 #Напишите программу, которая принимает на вход два числа и
 #проверяет, является ли одно число квадратом другого.
 
-print('input 5 digits divided with ,')
-a = input()
-b = a.split(',')
-print(b)
-c = []
-for tmp in b:
-    c.append(int(tmp))
-print(max(c))
+print('input а')
+a = float(input())
+b = int(a*10)
+print(b % 10)
+
+#b = a.split(',')
+#print(b)
+#c = []
+#for tmp in b:
+#    c.append(int(tmp))
+#print(max(c))
 #print('input b')
 #b = int(input())
 #result = 'no'
@@ -16,11 +19,13 @@ print(max(c))
 #    result = 'yes'
 #print(result)
 
-max_number = float('-inf')
 
-for i in range(1, 6):
-    number = int(input(f'Введите число {i}: '))
-    if number > max_number:
-        max_number = number
 
-print(max_number)
+
+#max_number = float('-inf')
+#for i in range(1, 6):
+#    number = int(input(f'Введите число {i}: '))
+#    if number > max_number:
+#        max_number = number
+
+#print(max_number)
