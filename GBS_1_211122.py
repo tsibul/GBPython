805b5e6e57faeadac5e8fd47794f40dd84c3377f
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-21 21:21:29 +0300

    middle commit

---
+++ GBS_1_211122.py
@@ -0,0 +1,26 @@
+#Напишите программу, которая принимает на вход два числа и
+#проверяет, является ли одно число квадратом другого.
+
+print('input 5 digits divided with ,')
+a = input()
+b = a.split(',')
+print(b)
+c = []
+for tmp in b:
+    c.append(int(tmp))
+print(max(c))
+#print('input b')
+#b = int(input())
+#result = 'no'
+#if a == b**2 or b == a**2:
+#    result = 'yes'
+#print(result)
+
+max_number = float('-inf')
+
+for i in range(1, 6):
+    number = int(input(f'Введите число {i}: '))
+    if number > max_number:
+        max_number = number
+
+print(max_number)
