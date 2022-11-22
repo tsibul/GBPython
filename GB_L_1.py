a0cf216950b72b1f72e3252a915fecea3cd09985
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-22 23:17:32 +0300

    Initial commit

---
+++ GB_L_1.py
@@ -0,0 +1,12 @@
+a = 123
+b = 1.23
+print(a, b)
+print(type(b))
+print(type(None))
+print(' hello', '\t', 'world')
+
+print('input a')
+a = input()
+print('input b')
+b = input()
+print(a, ' + ', b, ' = ', a+b)
---
+++ main.py
@@ -0,0 +1,6 @@
+import cv2
+
+
+img = cv2.imread('test.jpeg')
+print(img)
+
