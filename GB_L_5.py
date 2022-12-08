7fcd553abd9cebc6176784ea0b5eae6d5df3b959
Ilya Tsibul <tsigel@vikivostok.ru>
2022-12-08 15:47:17 +0300

    spreads

--- GB_L_5.py
+++ GB_L_5.py
@@ -56,10 +56,36 @@ print(*arr)
 #    [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
 
 
-array = [1, 5, 2, 3, 4, 9, 6, 1, 7, 8, 10]
-for i in range(1, len(array) - 1):
-    arr_n = [array[0]]
-    for j in range(i, len(array)):
-        if array[j] > arr_n[-1]:
-            arr_n.append(array[j])
-    print(arr_n)
+array = [1, 5, 2, 3, 4, 9, 6, 1, 6, 7, 8, 10]
+
+length = len(array)
+spread_collection = {}
+for i in range(length):
+    spread_collection.update({i: [[array[i]]]})
+
+print(spread_collection)
+
+for i in range(length):
+    tmp_item = spread_collection[i]
+    tmp_small_item = []
+    for small_item in tmp_item:
+        for j in range(i + 1, length):
+            if array[j] > small_item[-1]:
+                tmp_small_item = small_item.copy()
+                tmp_small_item.append(array[j])
+                spread_collection[j].append(tmp_small_item)
+tmp_length = 0
+for v in spread_collection:
+    print(v, spread_collection[v])
+    for item in spread_collection[v]:
+        if len(item) > tmp_length:
+            tmp_arr = []
+            tmp_arr.append(item)
+            tmp_length = len(item)
+        elif len(item) == tmp_length:
+            tmp_arr.append(item)
+print(tmp_length)
+print(tmp_arr)
+
+
+
